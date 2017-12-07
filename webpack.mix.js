2221b32a11d119dd6c4ffb7e581edc8bf77b1c88
Adrien Leloup <adrien@whitecube.be>
2017-12-07 16:10:01 +0100

    Added styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
 mix.js('assets/js/script.js', 'dist')
-   .sass('assets/scss/styles.scss', 'dist')
+   .sass('assets/scss/admin.scss', 'dist')
    .copy('assets/fonts', 'dist')
    .setPublicPath('dist');
