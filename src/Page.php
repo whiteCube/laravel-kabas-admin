4eed4d5566d7be91c2a81db25376cfa4c3b93d8a
Adrien Leloup <adrien@whitecube.be>
2017-12-11 16:09:44 +0100

    Fixed page save

--- src/Page.php
+++ src/Page.php
@@ -45,24 +45,37 @@ class Page {
         $values = [];
         foreach(Admin::locales() as $locale) {
             $values[$locale] = $this->loadValue($locale);
-            $this->setMetadata($values[$locale], $locale);
-            $this->insertIntoFields($values[$locale], $locale);
+            $this->setMetadata($values[$locale]->title, $values[$locale]->meta, $locale);
+            $this->insertIntoFields((object) $values[$locale], $locale);
         }
     }
 
-    protected function setMetadata($data, $locale)
+    protected function setMetadata($title = '', $meta = [], $locale)
     {
-        $this->title[$locale] = $data->title;
-        $this->meta[$locale] = $data->meta;
+        $this->title[$locale] = $title;
+        $this->meta[$locale] = $meta;
     }
 
     protected function insertIntoFields($values, $locale)
     {
+        if(!isset($values->meta)) $values->meta = $this->extractMetaValues($values);
+        $this->setMetadata($values->title, $values->meta, $locale);
         foreach($values as $key => $value) {
-            if($key === 'title') $this->title[$locale] = $value;
+            if($key === 'title' || $key === 'meta') continue;
             if(!isset($this->fields->$key)) continue;
             $this->fields->$key->setValue($value, $locale);
         }
+    }
+
+    protected function extractMetaValues($values)
+    {
+        $meta = [];
+        foreach($values as $key => $value) {
+            if(strpos($key, 'meta#') !== false) {
+                $meta[str_replace('meta#', '', $key)] = $value;
+            }
+        }
+        return $meta;
     }
 
     protected function loadValue($locale)
@@ -78,17 +91,32 @@ class Page {
 
     public function setValues($values)
     {
-        dd($values);
         foreach($values as $lang => $data) {
-            $this->insertIntoFields($data, $lang);
+            $this->insertIntoFields((object) $data, $lang);
         }
     }
 
     public function save()
     {
-        foreach($this->values as $lang => $data) {
-            Storage::disk('admin_values')->put($lang . '/static/' . $this->file, json_encode($data));
+        foreach($this->getValues() as $lang => $data) {
+            $oldValues = json_decode(Storage::disk('admin_values')->get($lang . '/static/' . $this->file), true);
+            $values = array_replace_recursive($oldValues, (array) $data);
+            Storage::disk('admin_values')->put($lang . '/static/' . $this->file, json_encode($values, JSON_PRETTY_PRINT));
         }
+    }
+
+    protected function getValues()
+    {
+        $values = new \stdClass;
+        foreach(Admin::locales() as $locale) {
+            $values->$locale = new \stdClass;
+            $values->$locale->title = $this->title[$locale];
+            $values->$locale->meta = $this->meta[$locale];
+            foreach($this->fields as $key => $field) {
+                $values->$locale->$key = $field->value($locale);
+            }
+        }
+        return $values;
     }
 
     public function lastModified()
@@ -104,16 +132,26 @@ class Page {
     public function metaGroupStructure($lang)
     {
         $structure = json_decode(file_get_contents(__DIR__ . '/' . $this->defaultFields));
-        if(!isset($this->config->meta)) return htmlentities(json_encode($structure));
-        $structure->meta = (object) [
-            "type" => "group",
-            "label" => "Meta",
-            "options" => (object) []
-        ];
-        foreach($this->config->meta as $key => $field) {
+        $structure = $this->makeTranslated($structure, $lang);
+        if(!isset($this->config->meta)) {
+            unset($structure->meta);
+            return htmlentities(json_encode($structure));
+        }
+        $meta = $this->makeTranslated($this->config->meta, $lang, 'meta');
+        foreach($meta as $key => $field) {
             $structure->meta->options->$key = $field;
         }
         return htmlentities(json_encode($structure));
+    }
+
+    protected function makeTranslated($structure, $lang, $prefix = false)
+    {
+        foreach($structure as $key => $field) {
+            $append = "$lang|";
+            if($prefix) $append .= "$prefix#";
+            $structure->$key->name = $append . $key;
+        }
+        return $structure;
     }
 
     public function metaGroupValues($lang)
