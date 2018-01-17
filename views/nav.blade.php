2025a71f96df5f812caa1cbc2e0b0faf82874b7d
Adrien Leloup <adrien@whitecube.be>
2018-01-17 15:31:31 +0100

    Fixed class names to avoid conflicts

--- views/nav.blade.php
+++ views/nav.blade.php
@@ -1,15 +1,15 @@
-<nav class="nav">
-    <h2 class="nav__title">Main navigation</h2>
-    <a class="nav__link nav__link--primary {{ url()->current() == route('kabas.admin') ? 'nav__link--active' : '' }}" href="{{ route('kabas.admin') }}">Administration</a>
-    <em class="nav__subtitle">Pages</em>
+<nav class="kabasnav">
+    <h2 class="kabasnav__title">Main navigation</h2>
+    <a class="kabasnav__link kabasnav__link--primary {{ url()->current() == route('kabas.admin') ? 'kabasnav__link--active' : '' }}" href="{{ route('kabas.admin') }}">Administration</a>
+    <em class="kabasnav__subtitle">Pages</em>
     @foreach (Admin::pages() as $page)
-        <a class="nav__link {{ url()->current() == route('kabas.admin.page', ['file' => $page->url]) ? 'nav__link--active' : '' }}" href="{{ route('kabas.admin.page', ['file' => $page->url]) }}">
+        <a class="kabasnav__link {{ url()->current() == route('kabas.admin.page', ['file' => $page->url]) ? 'kabasnav__link--active' : '' }}" href="{{ route('kabas.admin.page', ['file' => $page->url]) }}">
             {{ $page->name }}
         </a>
     @endforeach
-    <em class="nav__subtitle">Modèles</em>
+    <em class="kabasnav__subtitle">Modèles</em>
     @foreach (Admin::models() as $model)
-        <a class="nav__link {{ url()->current() == route('kabas.admin.model', ['file' => $model->file]) ? 'nav__link--active' : '' }}" href="{{ route('kabas.admin.model', ['file' => $model->file]) }}">
+        <a class="kabasnav__link {{ url()->current() == route('kabas.admin.model', ['file' => $model->file]) ? 'kabasnav__link--active' : '' }}" href="{{ route('kabas.admin.model', ['file' => $model->file]) }}">
             {{ $model->name }}
         </a>
     @endforeach
